db7d1efa3c3b1ac826b8d429b183399fa6811989
yucanwu <freedomvocano@gmail.com>
2026-09-16 02:41:13 -0500

    Add site interactions

---
+++ assets/site.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener('click',e=>{const id=a.getAttribute('href');if(id.length>1){const el=document.querySelector(id);if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'});}}});});
